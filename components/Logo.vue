2158d9c625be4c0fa201b825c8bcbbb67baae82b
axelinternet <axel@hultman.info>
2017-11-08 15:27:35 +0100

    init

---
+++ components/Logo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ArmadaLogoHolder">
+    <img src="http://armada.nu/assets/logo.png" class="mainLogo" />
+  </div>
+</template>
+
+<style>
+.ArmadaLogoHolder {
+  display: inline-block;
+  position: relative;
+  height: 180px;
+  width: 245px;
+}
+
+
+.mainLogo {
+  height: auto;
+  width: inherit;
+}
+@keyframes turn {
+  100% {
+    transform: rotateX(0deg);
+  }
+}
+
+@keyframes godown {
+  100% {
+    top: 180px;
+  }
+}
+
+@keyframes goright {
+  100% {
+    left: 70px;
+  }
+}
+</style>
